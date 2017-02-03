7264d607f28380259dae8b47d0fa8772c8ebf4e9
vikash12345 <vikash@backgroundcheck.email>
2017-02-03 11:14:18 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,24 +1,12 @@
-
-
-
-
-
 <?php
 require 'scraperwiki.php';
-
 require 'scraperwiki/simple_html_dom.php';
- $MyWebsite = 'http://www.pmdc.org.pk//DesktopModules/pmdcDetails/PractDetail.aspx?RegistrationNo=9010-N';
- $html  = file_get_html($MyWebsite);
+$MyWebsite = 'https://www.secp.gov.pk/company-name-search/?searchCriteria=Start&companyName=z';
+$html  = file_get_html($MyWebsite);
+foreach($html->find("//*[@id="address-list"]/div[1]/h4")as $element){
+$Com = $element->find("h4", 0)->plaintext;
 
- foreach($html->find("//*[@class='cssSTbl']/tbody/tr") as $element) {
-  if ($element) {
-   $TD_Title = $element->find("td", 0);
-   $TD_Value = $element->find("td", 1);
-   if ($TD_Title && $TD_Value) {
-    print $TD_Title . ' --> ' . $TD_Value;
-    echo '<br/>';
-   }
-  }
- }
-  
+print $Com;
+echo '<br/>';
+}
 ?>
