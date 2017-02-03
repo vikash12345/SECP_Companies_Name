8c44f84d39bb8e4efdfe209c1cc8c63405810558
vikash12345 <vikash@backgroundcheck.email>
2017-02-03 11:21:23 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,26 +1,12 @@
 <?php
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
-$MyWebsite = 'http://www.pmdc.org.pk//DesktopModules/pmdcDetails/PractDetail.aspx?RegistrationNo=';
+$MyWebsite = 'https://www.secp.gov.pk/company-name-search/?searchCriteria=Start&companyName=z';
+$html  = file_get_html($MyWebsite);
+foreach($html->find("//*[@id="address-list"]/div[1]/h4")as $element){
+$Com = $element->find("h4")->plaintext;
 
- for ($serialnumb = 9000; $serialnumb < 9020; $serialnumb++) {
-  $NewURL = $MyWebsite . $serialnumb . '-N';
-  $html = file_get_html($NewURL);
-  
-  echo 'User ID #: ' . $serialnumb . '-N';
-  echo '<br/>';
-  
-  foreach($html->find("//*[@class='cssSTbl']/tbody/tr") as $element) {
-   if ($element) {
-    $TD_Title = $element->find("td", 0);
-    $TD_Value = $element->find("td", 1);
-    if ($TD_Title && $TD_Value) {
-     print $TD_Title->plaintext . ' --> ' . $TD_Value->plaintext;
-     echo '<br/>';
-    }
-   }
-  }
-  echo '-------------------------------------------<br/>';
- }
-
+print $Com;
+echo '<br/>';
+}
 ?>
