e90e5c9dbc7e5543f3e4825a5d38ca25fc48cf76
vikash12345 <vikash@backgroundcheck.email>
2017-02-03 11:26:08 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -3,8 +3,8 @@ require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 $MyWebsite = 'https://www.secp.gov.pk/company-name-search/?searchCriteria=Start&companyName=z';
 $html  = file_get_html($MyWebsite);
-foreach($html->find("//*[@id="address-list"]/div[1]/h4")as $element){
-$Com = $element->find("h4")->plaintext;
+foreach($html->find("//*[@id='address-list']/div[1]/h4")as $element){
+$Com = $element->find("h4",0)->plaintext;
 
 print $Com;
 echo '<br/>';
