2520feec2229a4e2bc2c45269817d166bfa64878
vikash12345 <vikash@backgroundcheck.email>
2017-02-03 11:07:03 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,38 +1,24 @@
-<?
-// This is a template for a PHP scraper on morph.io (https://morph.io)
-// including some code snippets below that you should find helpful
 
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
-//
-// // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
-//
-// // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
-//
-// // Write out to the sqlite database using scraperwiki library
-// scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
-//
-// // An arbitrary query against the database
-// scraperwiki::select("* from data where 'name'='peter'")
 
-// You don't have to do things with the ScraperWiki library.
-// You can use whatever libraries you want: https://morph.io/documentation/php
-// All that matters is that your final data is written to an SQLite database
-// called "data.sqlite" in the current working directory which has at least a table
-// called "data".
-?>
- <?php
+
+
+
+<?php
 require 'scraperwiki.php';
+
 require 'scraperwiki/simple_html_dom.php';
-foreach($html->find("//*[@id="address-list"]/div[1]/h4")as $element)
+ $MyWebsite = 'http://www.pmdc.org.pk//DesktopModules/pmdcDetails/PractDetail.aspx?RegistrationNo=9010-N';
  $html  = file_get_html($MyWebsite);
-foreach($html->find(".listing-single") as $element){
-$Com = $element->find("h4", 0)->plaintext;
-print $Com;  
-echo '<br/>';
-}
+
+ foreach($html->find("//*[@class='cssSTbl']/tbody/tr") as $element) {
+  if ($element) {
+   $TD_Title = $element->find("td", 0);
+   $TD_Value = $element->find("td", 1);
+   if ($TD_Title && $TD_Value) {
+    print $TD_Title . ' --> ' . $TD_Value;
+    echo '<br/>';
+   }
+  }
+ }
+  
 ?>
