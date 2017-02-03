4757ea8270f0a72a87e059804c2520dbf6ea55ff
vikash12345 <vikash@backgroundcheck.email>
2017-02-03 12:21:16 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,13 +1,9 @@
 <?php
-require 'scraperwiki.php';
+
+require ''scraperwiki.php;
 require 'scraperwiki/simple_html_dom.php';
- $MyWebsite = 'https://www.secp.gov.pk/wp-content/themes/secp/company-results.php?companyName=z&searchCriteria=Start';
- $xml  = simplexml_load_file($MyWebsite);
- foreach($xml as $entry) {
-  foreach($entry->CompanyInfo as $item) {
-   $items['name'] = (string) $item->Name;
-   $items['address'] = (string) $item->OfficeAddress;
-   echo 'Name = ' . $items['name'] . '<br/>';
-  }
- }
+$site = 'https://www.secp.gov.pk/wp-content/themes/secp/company-results.php?companyName=z&searchCriteria=Start';
+$html = file_get_html($site);
+echo $html;
+
 ?>
