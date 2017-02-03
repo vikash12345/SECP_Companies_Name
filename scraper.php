059e69bb36a19c8fe064a92787baa5c3d36c22d0
vikash12345 <vikash@backgroundcheck.email>
2017-02-03 12:24:04 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,6 +1,6 @@
 <?php
 
-require ''scraperwiki.php;
+require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 $site = 'https://www.secp.gov.pk/wp-content/themes/secp/company-results.php?companyName=z&searchCriteria=Start';
 $html = file_get_html($site);
